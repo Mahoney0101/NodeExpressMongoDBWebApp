701a2b55f417ae0d175e81f50169516dee334d58
Mahoney0101 <43443867+Mahoney0101@users.noreply.github.com>
2019-10-10 10:21:30 +0100

    update

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV === 'production') {
     dbURI = process.env.MONGOLAB_URI;
 }
 
-mongoose.connect(String(dbURI),{ useNewUrlParser: true, useUnifiedTopology: true  });
+mongoose.connect(dbURI);
 
 // CONNECTION EVENTS
 mongoose.connection.on('connected', function() {
