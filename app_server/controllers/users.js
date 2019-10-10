a9dde0b9a8fceef23a2dd08f99a695d46f416da1
Mahoney0101 <43443867+Mahoney0101@users.noreply.github.com>
2019-10-10 13:58:50 +0100

    update

--- app_server/controllers/users.js
+++ app_server/controllers/users.js
@@ -5,34 +5,34 @@ const apiOptions = {
 if (process.env.NODE_ENV === 'production') {
   apiOptions.server = 'https://fast-reef-45489.herokuapp.com/';
 }
- const createUser = console.log("HELLOO");
-//function(req, res) {
+ 
+function(req, res) {
    
-//     const path = `/api/users`;
-//     const postdata = {
-//       name: req.body.name,
-//       email: req.body.email,
-//       password: req.body.password
-//     };
+    const path = `/api/users`;
+    const postdata = {
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    };
 
-//     const requestOptions = {
-//       url : apiOptions.server + path,
-//       method : 'POST',
-//       json : postdata
-//     };
-//       request(
-//         requestOptions,
-//         (err, response, body) => {
-//           if (response.statusCode === 201) {
-//             res.redirect(`/login`);
-//           } else if (response.statusCode === 400 && body.name && body.name === 'ValidationError' ) {
-//             res.redirect(`/register/err=val`);
-//           } else {
-//             _showError(req, res, response.statusCode);
-//           }
-//         }
-//       );
-//     };
+    const requestOptions = {
+      url : apiOptions.server + path,
+      method : 'POST',
+      json : postdata
+    };
+      request(
+        requestOptions,
+        (err, response, body) => {
+          if (response.statusCode === 201) {
+            res.redirect(`/login`);
+          } else if (response.statusCode === 400 && body.name && body.name === 'ValidationError' ) {
+            res.redirect(`/register/err=val`);
+          } else {
+            _showError(req, res, response.statusCode);
+          }
+        }
+      );
+    };
 
   module.exports = {createUser};
   
